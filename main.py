544118a06181e8a81663cfcde68d486471fa66db
Abinaya Priyadharshini <abinayasiva29@gmail.com>
2026-02-04 15:03:27 +0530

    Add simple input and print program

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-if age >= 18:
-     print("Hello",name + "! You are an adult.")
-else:
-     print("Hello", name + "! You are a minor.")
+print("Hello", name)
 
